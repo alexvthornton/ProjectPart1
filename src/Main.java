b198640d25e94b70f177983e032adeaa2299849d
Alex Thornton <athornton4@eagles.ewu.edu>
2020-11-16 13:55:01 -0800

    intital commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import cs350f20project.Startup;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Startup su = new Startup();
+        su.go();
+    }
+
+}
---
+++ src/cs350f20project/controller/cli/parser/CommandParser.java
@@ -0,0 +1,12 @@
+package cs350f20project.controller.cli.parser;
+
+public class CommandParser {
+
+    public CommandParser(MyParserHelper parserHelper, String commandText){
+
+    }
+
+    public void parse(){
+        System.out.println("our parser!");
+    }
+}
