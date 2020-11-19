cbd387370ef63719fa6501b0878ae714eb3d8747
Alex Thornton <athornton4@eagles.ewu.edu>
2020-11-19 12:20:20 -0800

    broke down create command further

--- src/cs350f20project/controller/cli/parser/CreateCommandParser.java
+++ src/cs350f20project/controller/cli/parser/CreateCommandParser.java
@@ -3,15 +3,16 @@ package cs350f20project.controller.cli.parser;
 public class CreateCommandParser extends CommandParser {
 
     private String commandText;
+    private String[] commandArr;
 
     protected CreateCommandParser(MyParserHelper parserHelper, String commandText) {
         super(parserHelper, commandText);
         this.commandText = commandText;
+        this.commandArr = commandText.split(" ");
     }
 
     public void parse(){
 
-        String[] commandArr = commandText.split(" ");
         if(commandArr[1].equals("power")){
             power();
         }
@@ -25,7 +26,22 @@ public class CreateCommandParser extends CommandParser {
 
     private void power(){
 
+        CreatePowerCommandParser power = new CreatePowerCommandParser(super.parserHelper, commandText);
+
+        if(commandArr[2].equals("catenary")){
+            power.parseCatenary();
+        }
+        else if(commandArr[2].equals("pole")){
+            power.parsePole();
+        }
+        else if(commandArr[2].equals("station")){
+            power.parseStation();
+        }
+        else if(commandArr[2].equals("substation")){
+            power.parseSubstation();
+        }
     }
+
 
     private void stock(){
 
---
+++ src/cs350f20project/controller/cli/parser/CreatePowerCommandParser.java
@@ -0,0 +1,25 @@
+package cs350f20project.controller.cli.parser;
+
+public class CreatePowerCommandParser extends CreateCommandParser{
+
+
+    protected CreatePowerCommandParser(MyParserHelper parserHelper, String commandText) {
+        super(parserHelper, commandText);
+    }
+
+    public void parseCatenary(){
+
+    }
+
+    public void parsePole(){
+
+    }
+
+    public void parseStation(){
+
+    }
+
+    public void parseSubstation(){
+
+    }
+}
